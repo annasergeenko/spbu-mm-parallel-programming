6a4ab35b831a612590e97d1bac9b2c0a5603ebc8
Ivan Tyulyandin <ivan.tyulyandin@gmail.com>
2020-03-12 21:00:19 +0300

    Add producer/consumer config runner

---
+++ task_1/config_runner.h
@@ -0,0 +1,103 @@
+#include <vector>
+#include <future>
+#include <iostream>
+#include <algorithm>
+
+#include "queue_on_list.h"
+#include "producer.h"
+#include "consumer.h"
+
+// also used for producers' data generation in run_config
+using Data_type     = int;
+using Data_type_vec = std::vector<Data_type>;
+
+class Config {
+public:
+    std::string name = "Unknown";
+
+    int num_of_prods;
+    int prod_elem_amount;
+    std::chrono::seconds pause_for_producers;
+
+    int num_of_cons;
+    std::chrono::seconds pause_for_consumers;
+
+    using Delay_function = void(*)();
+    Delay_function delay_func;
+};
+
+
+bool run_config(const Config& cfg) {
+    auto producers = std::vector<Producer<Data_type>>();
+    producers.reserve(cfg.num_of_prods);
+
+    for (Data_type i = 0; i < cfg.num_of_prods; ++i) {
+        producers.emplace_back(Producer<Data_type>(
+                Data_type_vec(cfg.prod_elem_amount, i), cfg.pause_for_producers));
+    }
+
+    auto consumers = std::vector<Consumer<Data_type>>();
+    consumers.reserve(cfg.num_of_cons);
+
+    for (Data_type i = 0; i < cfg.num_of_cons; ++i) {
+        consumers.emplace_back(Consumer<Data_type>(cfg.pause_for_consumers));
+    }
+
+    auto queue = Queue_on_list<Data_type>();
+    bool should_stop = false;
+
+    auto threads_for_producers = std::vector<std::future<void>>();
+    threads_for_producers.reserve(producers.size());
+
+    for (auto& prod : producers) {
+        threads_for_producers.push_back(
+                std::async(std::launch::async, &Producer<Data_type>::writing_to_queue_until, &prod,
+                           std::ref(queue), std::ref(should_stop)));
+    }
+
+    auto threads_for_consumers = std::vector<std::future<void>>();
+    threads_for_consumers.reserve(consumers.size());
+
+    for (auto& cons : consumers) {
+        threads_for_consumers.push_back(
+                std::async(std::launch::async, &Consumer<Data_type>::reading_from_queue_until, &cons,
+                           std::ref(queue), std::ref(should_stop)));
+    }
+
+    cfg.delay_func();
+    should_stop = true;
+
+    for (auto& thr : threads_for_producers) {
+        thr.get();
+    }
+
+    for (auto& thr : threads_for_consumers) {
+        thr.get();
+    }
+
+    auto all_prods_written_data = Data_type_vec{};
+    for (auto& prod : producers) {
+        auto data = prod.get_written_data();
+        all_prods_written_data.insert(all_prods_written_data.end(), data.begin(), data.end());
+    }
+
+    auto all_cons_read_data = Data_type_vec{};
+    for (auto& cons : consumers) {
+        auto data = cons.get_read_data();
+        all_cons_read_data.insert(all_cons_read_data.end(), data.begin(), data.end());
+    }
+
+    auto unread_data = queue.get_data();
+    all_cons_read_data.insert(all_cons_read_data.end(), unread_data.begin(), unread_data.end());
+
+    std::sort(all_prods_written_data.begin(), all_prods_written_data.end());
+    std::sort(all_cons_read_data.begin(), all_cons_read_data.end());
+
+    auto run_result = all_prods_written_data == all_cons_read_data;
+    if (!run_result) {
+        std::cout << '\"' << cfg.name << "\" failed\n";
+    } else {
+        std::cout << '\"' << cfg.name << "\" passed\n";
+    }
+    return run_result;
+}
--- task_1/main.cpp
+++ task_1/main.cpp
@@ -1,85 +1,26 @@
-#include <vector>
-#include <future>
-#include <iostream>
-#include <algorithm>
+#include "config_runner.h"
 
-#include "queue_on_list.h"
-#include "producer.h"
-#include "consumer.h"
+constexpr int NUM_OF_PRODUCERS = 5;
+constexpr int NUM_OF_CONSUMERS = 5;
+constexpr int ELEM_AMOUNT      = 100;
 
-constexpr auto NUM_OF_PRODUCERS = 5;
-constexpr auto NUM_OF_CONSUMERS = 5;
-constexpr auto ELEM_AMOUNT      = 100;
 
-using Data_type     = int;
-using Data_type_vec = std::vector<Data_type>;
+void wait_for_enter_and_return() {
+    std::cout << "Press Enter to stop work...\n";
+    std::string tmp;
+    getline(std::cin, tmp);
+    std::cout << "Waiting for threads...\n";
+}
+
 
 int main() {
-
-    auto producers = std::vector<Producer<Data_type>>();
-    producers.reserve(NUM_OF_PRODUCERS);
-
-    for (Data_type i = 0; i < NUM_OF_PRODUCERS; ++i) {
-        producers.emplace_back(Producer<Data_type>(
-                Data_type_vec(ELEM_AMOUNT, i), std::chrono::seconds(i)));
-    }
-
-    auto consumers = std::vector<Consumer<Data_type>>();
-    consumers.reserve(NUM_OF_CONSUMERS);
-
-    for (Data_type i = 0; i < NUM_OF_CONSUMERS; ++i) {
-        consumers.emplace_back(Consumer<Data_type>(std::chrono::seconds(i)));
-    }
-
-    auto queue = Queue_on_list<Data_type>();
-    bool should_stop = false;
-    auto threads_producers = std::vector<std::future<void>>();
-    auto threads_consumers = std::vector<std::future<void>>();
-
-    for (auto& prod : producers) {
-        threads_producers.push_back(
-                std::async(std::launch::async, &Producer<Data_type>::writing_to_queue_until, &prod,
-                        std::ref(queue), std::ref(should_stop)));
-    }
-
-    for (auto& cons : consumers) {
-        threads_consumers.push_back(
-                std::async(std::launch::async, &Consumer<Data_type>::reading_from_queue_until, &cons,
-                           std::ref(queue), std::ref(should_stop)));
-    }
-
-    std::cout << "Press Enter to stop threads...\n";
-    std::string tmp{};
-    getline(std::cin, tmp);
-    should_stop = true;
-    std::cout << "Waiting for threads...\n";
-
-    for (auto& thr : threads_producers) {
-        thr.get();
-    }
-
-    for (auto& thr : threads_consumers) {
-        thr.get();
-    }
-
-    auto all_prods_written_data = Data_type_vec{};
-    for (auto& prod : producers) {
-        auto data = prod.get_written_data();
-        all_prods_written_data.insert(all_prods_written_data.end(), data.begin(), data.end());
-    }
-
-    auto all_cons_read_data = Data_type_vec{};
-    for (auto& cons : consumers) {
-        auto data = cons.get_read_data();
-        all_cons_read_data.insert(all_cons_read_data.end(), data.begin(), data.end());
-    }
-
-    std::sort(all_prods_written_data.begin(), all_prods_written_data.end());
-    std::sort(all_cons_read_data.begin(), all_cons_read_data.end());
-
-    if (all_prods_written_data != all_cons_read_data) {
-        std::cout << "Failed\n";
-    } else {
-        std::cout << "Correct\n";
-    }
+    auto cfg = Config{};
+    cfg.name = "Run until enter pressed";
+    cfg.num_of_prods = NUM_OF_PRODUCERS;
+    cfg.prod_elem_amount = ELEM_AMOUNT;
+    cfg.pause_for_producers = static_cast<std::chrono::seconds>(1);
+    cfg.num_of_cons = NUM_OF_CONSUMERS;
+    cfg.pause_for_consumers = static_cast<std::chrono::seconds>(1);
+    cfg.delay_func = wait_for_enter_and_return;
+    run_config(cfg);
 }
