6e238dd19c4eef0f97ce0d6b444ce88274837a4a
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2020-04-26 21:45:34 +0300

    Initial commit for ThreadPool project

---
+++ ThreadPool/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    kotlin("jvm") version "1.3.72"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.6.2")
+    testImplementation("org.jetbrains.kotlin:kotlin-test:1.3.72")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
